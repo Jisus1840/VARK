a3669f35e2d0bf3149714c525ac92082d0f09568
Jisus1840 <72171084+Jisus1840@users.noreply.github.com>
2022-11-21 16:56:00 -0600

    no funciona booststrap

--- resources/views/layouts/base_html.blade.php
+++ resources/views/layouts/base_html.blade.php
@@ -10,7 +10,10 @@
     @include('modal.modal')
 
     {{-- BOOSTRAP 5 --}}
-    <link rel="stylesheet" href="{{ asset('lib/bootstrap5/css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('lib/bootstrap5/css/bootstrap.min.css') }}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 
     {{-- DATATABLES --}}
     <!-- <link rel="stylesheet" href="{{ asset('lib/dataTables/datatables.bootstrap5.min.css') }}">
@@ -26,7 +29,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     {{-- ESTILOS PROPIOS --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
 
 </head>
 
@@ -47,7 +50,11 @@
 </body>
 @stack('scripts')
 {{-- BOOSTRAP 5 JS --}}
-<script src="{{ asset('lib/bootstrap5/js/bootstrap.bundle.min.js') }}"></script>
+{{-- <script src="{{ asset('lib/bootstrap5/js/bootstrap.bundle.min.js') }}"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
 
 {{-- JQUERY --}}
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
