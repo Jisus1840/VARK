4064bfbbc7ceef5bbc5f7c5483455a53e3d6c45f
erick <delabra32@gmail.com>
2022-11-21 19:23:21 -0600

    nadaaaaa2

--- resources/views/layouts/base_html.blade.php
+++ resources/views/layouts/base_html.blade.php
@@ -10,7 +10,7 @@
     @include('modal.modal')
 
     {{-- BOOSTRAP 5 --}}
-    {{-- <link rel="stylesheet" href="{{ asset('lib/bootstrap5/css/bootstrap.min.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('lib/bootstrap5/css/bootstrap.min.css') }}"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -25,8 +25,8 @@
     {{-- swal alert libreria --}}
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     {{-- ESTILOS PROPIOS --}}
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
